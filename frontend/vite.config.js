aa295471256b7862313721de332649d5db40a80f
klaus-0-0 <jin0667666@gmail.com>
2025-12-25 16:30:06 +0530

    Reorder plugins and add server configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    host: true,
+    allowedHosts: ['ilets.onrender.com'], 
+  },
+})  
